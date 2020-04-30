91fd99465fb688023f828067440f7b3d279b683a
Allen Zhang <allenz9410@gmail.com>
2020-04-30 22:49:23 +0000

    Change kmer length to 16

--- src/config.rs
+++ src/config.rs
@@ -19,7 +19,7 @@ pub const U32_MAX: usize = u32::max_value() as usize;
 // Worker queue configs
 pub const MAX_WORKER: usize = 2;
 
-pub type KmerType = kmer::Kmer12;
+pub type KmerType = kmer::Kmer16;
 
 // Transcriptome mappability
 pub const MAPPABILITY_COUNTS_LEN: usize = 11;
